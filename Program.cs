81086f09ccdef63724cfd183eda297b7b95bb87e
Lauren Goss <laurenlgoss98@gmail.com>
2022-01-12 16:29:31 -0500

    added methods for getting/printing employees to CLI

--- Program.cs
+++ Program.cs
@@ -1,1 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.Collections.Generic;
+
+namespace CatWorx.BadgeMaker
+{
+    class Program
+    {
+        // Return list of employees inputted by user
+        static List<string> GetEmployees()
+        {
+            List<string> employees = new List<string>();
+            while (true)
+            {
+                Console.WriteLine("Please enter a name (leavy empty to exit):");
+                string input = Console.ReadLine();
+                if (input == "")
+                {
+                    break;
+                }
+                employees.Add(input);
+            }
+            return employees;
+        }
+        // Prints inputted employee list to CLI
+        static void PrintEmployees(List<string> employees)
+        {
+            for (int i = 0; i < employees.Count; i++)
+            {
+                Console.WriteLine(employees[i]);
+            }
+        }
+        // Entry point for application
+        static void Main(string[] args)
+        {
+            List<string> employees = GetEmployees();
+            PrintEmployees(employees);
+        }
+    }
+}
