f0d4b9ee5e2b9e49ccfe7959a5b8b534733387b8
Lauren Goss <laurenlgoss98@gmail.com>
2022-08-22 19:18:53 -0400

    added csv file logic

---
+++ Util.cs
@@ -0,0 +1,37 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace CatWorx.BadgeMaker
+{
+    class Util
+    {
+        // Prints inputted employee list to CLI
+        public static void PrintEmployees(List<Employee> employees)
+        {
+            for (int i = 0; i < employees.Count; i++) 
+            {
+                string template = "{0,-10}\t{1,-20}\t{2}";
+                Console.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetFullName(), employees[i].GetPhotoUrl()));
+            }
+        }
+
+        // Create CSV file from employees parameter
+        public static void MakeCSV(List<Employee> employees)
+        {
+            if (!Directory.Exists("data"))
+            {
+                Directory.CreateDirectory("data");
+            }
+            using (StreamWriter file = new StreamWriter("data/employees.csv"))
+            {
+                file.WriteLine("ID,Name,PhotoUrl");
+                for (int i = 0; i < employees.Count; i++) 
+                {
+                    string template = "{0},{1},{2}";
+                    file.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetFullName(), employees[i].GetPhotoUrl()));
+                }
+            }
+        }
+    }
+}
